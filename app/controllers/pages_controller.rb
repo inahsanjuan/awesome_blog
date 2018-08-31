a223996e01e16e889b7535d6ebad6c65e03b6604
Your Name <inahsanjuan@gmail.com>
2018-08-31 10:09:37 +0800

    Micropost improvements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     if current_user
       @micropost = current_user.microposts.new
+      @microposts = Micropost.all
       render "users/feed"
     end
   end
